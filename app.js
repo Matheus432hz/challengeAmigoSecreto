8c992987c372fb483a0b742661efdf5b9d3f84e0
matheus salla's laptop <matheus432hz@gmail.com>
2025-03-02 19:11:16 -0300

    percorrendo array e mostando ela como lista

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ let amigos = [];
 //funcao para agregar nomes
 function adicionarAmigo() {
     let nome = document.querySelector('input').value
-    if (nome.value = '') {
+    if (nome === '') {
         alert('nao deve estar em branco');//
     } else {
         amigos.push(nome);
         limparCampo();
         console.log(amigos);
+        listarAmigos();
     }
 }
 
-
+// funcao para limpar campo digitado, herado do jogo numero secreto
 function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
+}
+
+function listarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+    for (var i = 0; i < amigos.length; i++) {
+        let novoLi = document.createElement('li');
+        novoLi.textContent = amigos[i];
+        listaAmigos.appendChild(novoLi);
+    }
 }
