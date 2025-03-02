df1d5b847198d6ead7d4b62cdd41254138ffc582
matheus salla's laptop <matheus432hz@gmail.com>
2025-03-02 18:07:02 -0300

    function adicionar amigos

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // array/ lista de amigos
 let amigos = [];
 
+//funcao para agregar nomes
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value
+    if (nome.value = '') {
+        alert('nao deve estar em branco');//
+    } else {
+        amigos.push(nome);
+        limparCampo();
+        console.log(amigos);
+    }
+}
+
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
